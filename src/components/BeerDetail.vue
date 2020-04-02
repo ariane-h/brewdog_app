830d2c9b06580cb27bff84f4d4a537e6a2a24496
Ariane Holford <aribomop@gmail.com>
2020-04-02 13:15:18 +0100

    add extra info to beer details

--- src/components/BeerDetail.vue
+++ src/components/BeerDetail.vue
@@ -1,6 +1,18 @@
 <template lang="html">
   <div v-if="beer">
-      <h3>{{ beer.name }}</h3>
+      <h3>Name: {{ beer.name }}</h3>
+      <p>Tagline: {{ beer.tagline }}</p>
+      <p>ABV: {{ beer.abv }}%</p>
+
+      <img :src="beer.image_url" :alt="beer.name">
+
+      <p>Description: {{ beer.description }}</p>
+      <p>Food Pairings:</p>
+      <ul>
+        <li v-for="(food, index) in beer.food_pairing" :food="food" :key="index">{{ food }}</li>
+      </ul>
+
+
       
   </div>
 </template>
